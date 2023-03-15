d781a17cd86bbf6f63ae0fe9b1b9c8d534be6271
Juan <montessabogaljuan@icloud.com>
2023-03-15 17:23:50 -0500

    Agrega funcion restar a la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,9 @@
 def sumar(num1, num2):
     return num1 + num2
 
+def restar(num1, num2):
+    return num1 - num2
+
 num1 = 5
 num2 = 3
 operacion = "+"
@@ -8,4 +11,7 @@ operacion = "+"
 # Realizar la operación y mostrar el resultado
 if operacion == "+":
     resultado = num1 + num2
+    print("El resultado es:", resultado)
+elif operacion == "-":
+    resultado = num1 - num2
     print("El resultado es:", resultado)
